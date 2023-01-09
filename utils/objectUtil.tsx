23e69b20a4b225dacc85a3375650d4f19417728f
yuqil725 <yuqil@aipaca.ai>
2023-01-08 23:27:32 -0500

    Web version: profileItemArray auto create new item and delete empty item - Always have the first line empty

--- utils/objectUtil.tsx
+++ utils/objectUtil.tsx
@@ -19,3 +19,32 @@ export const objectMapKey = (o: any, map: any) => {
     };
   }, {});
 };
+
+export const objectAddEmptyStrToArray = (
+  o: any,
+  exception: string[] = ["imagePath", "education", "livingAddress"]
+) => {
+  return Object.keys(o).reduce((acc, key) => {
+    if (
+      Array.isArray(o[key]) &&
+      (o[key].length == 0 || o[key].at(0) != "") &&
+      !exception.includes(key)
+    ) {
+      const newValue = ["", ...o[key]];
+      return {
+        ...acc,
+        ...{ [key]: newValue },
+      };
+    }
+    return {
+      ...acc,
+      ...{ [key]: o[key] },
+    };
+  }, {});
+};
+
+export const objectRemoveEmptyStr = (o: string[]) => {
+  return o.filter((v: string) => {
+    return v.length > 0;
+  });
+};
